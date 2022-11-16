6f0ec81fc9e4c0ed73f788fb4e27af37e397e2d0
Hubert Smieciuch <hubert.smieciuch@gmail.com>
2022-11-16 22:49:08 +0100

    added functions for computations on radius distances

--- R/utils.R
+++ R/utils.R
@@ -64,3 +64,118 @@ get_agr <- function(data, grp_col, val_col, agr_type="mean"){
   data
 }
 
+
+
+#' Get matrix of radius distances
+#' @description Compute matrix of radius distances between points in two data.frames/data.table
+#'
+#' @param data1 data.frame or data.table containing lon and lat columns
+#' @param lonlat1 character vector of column names in data1 holding coordinates (longittude and lattitude)
+#' @param data2 optional data.frame or data.table containing lon and lat columns, if not given square matrix between points in data1 is computed
+#' @param lonlat2 character vector of column names in data2 holding coordinates (longittude and lattitude)
+#'
+#' @return data.table
+#' @export
+#'
+#' @examples
+get_rad_dist_matrix <- function(data1, lonlat1, data2=NULL, lonlat2=NULL){
+  data.table::setDT(data1)
+  data.table::setDT(data2)
+  if(is.null(data2)){
+    dist_matrix = geodist::geodist(data1[,..lonlat1], measure="geodesic")
+  } else {
+    dist_matrix = geodist::geodist(data1[,..lonlat1], data2[,..lonlat2], measure="geodesic")
+  }
+
+  dist_matrix = as.data.frame(dist_matrix)
+  data.table::setDT(dist_matrix)
+  return(dist_matrix)
+}
+
+
+
+#' Count number of points within radius distance
+#' @description Count number of points within radius distance of given number of kilometers
+#'
+#' @param radius radius km distance
+#' @param data1 data.frame or data.table containing lon and lat columns
+#' @param lonlat1 character vector of column names in data1 holding coordinates (longittude and lattitude)
+#' @param data2 optional data.frame or data.table containing lon and lat columns, if not given square matrix between points in data1 is computed
+#' @param lonlat2 character vector of column names in data2 holding coordinates (longittude and lattitude)
+#'
+#' @return data.table
+#' @export
+#'
+#' @examples
+count_in_rad <- function(radius, data1, lonlat1, data2=NULL, lonlat2=NULL){
+  dist_matrix <- get_rad_dist_matrix(data1, lonlat1, data2, lonlat2)
+  res <- rowSums(dist_matrix[, lapply(.SD, function(x) x<radius*1000), .SDcols=names(dist_matrix)])
+  return(res)
+}
+
+
+
+
+#' Find records within radius distance
+#'
+#' @param radius radius distance in km
+#' @param data1 data.frame or data.table containing lon and lat columns
+#' @param id1 character of column name holding identifiers in data1
+#' @param lonlat1 character vector of column names in data1 holding coordinates (longittude and lattitude)
+#' @param data2 optional data.frame or data.table containing lon and lat columns, if not given square matrix between points in data1 is computed
+#' @param id2 character of column name holding identifiers in data2
+#' @param lonlat2 character vector of column names in data2 holding coordinates (longittude and lattitude)
+#'
+#' @return data.table
+#' @export
+#'
+#' @examples
+get_n_in_rad <- function(radius, data1, id1, lonlat1, data2=NULL, id2=NULL, lonlat2=NULL){
+  dist_matrix <- get_rad_dist_matrix(data1, lonlat1, data2, lonlat2)
+  if(is.null(data2)) diag(dist_matrix)=NA
+  dist_matrix <- as.data.frame(t(dist_matrix))
+
+  id2 <- if(is.null(id2)) id1 else id2
+  data2 <- if(is.null(data2)) data1 else data2 # no need to transform to data.table?
+  res <- lapply(dist_matrix, function(x) {
+    within <- which(x < radius*1000)
+    temp <- cbind(data2[within,], data.frame(distance=round(x[within]/1000, 2)))
+    return(temp)
+  })
+  names(res) <- data1[[id1]]
+  return(res)
+}
+
+
+
+#' Find records given number of closests records from given one
+#'
+#' @param n number of closest locations to be returned
+#' @param data1 data.frame or data.table containing lon and lat columns
+#' @param id1 character of column name holding identifiers in data1
+#' @param lonlat1 character vector of column names in data1 holding coordinates (longittude and lattitude)
+#' @param data2 optional data.frame or data.table containing lon and lat columns, if not given square matrix between points in data1 is computed
+#' @param id2 character of column name holding identifiers in data2
+#' @param lonlat2 character vector of column names in data2 holding coordinates (longittude and lattitude)
+#'
+#' @return data.table
+#' @export
+#'
+#' @examples
+get_n_closest<- function(n, data1, id1, lonlat1, data2=NULL, id2=NULL, lonlat2=NULL){
+  dist_matrix <- get_rad_dist_matrix(data1, lonlat1, data2, lonlat2)
+  if(is.null(data2)) diag(dist_matrix)=NA
+  dist_matrix <- as.data.frame(t(dist_matrix))
+  id2 <- if(is.null(id2)) id1 else id2
+  data2 <- if(is.null(data2)) data1 else data2
+  res <- lapply(dist_matrix, function(x) {
+    treshhold <- sort(x)[n]
+    within <- which(x <= treshhold)
+    temp <- cbind(data2[within,], data.frame(distance=round(x[within]/1000, 2)))
+    data.table::setorder(temp, distance)
+    return(temp)
+  })
+  names(res) <- data1[[id1]]
+  return(res)
+}
+
