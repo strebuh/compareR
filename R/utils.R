cb2cf36353b1f560b5b91429f8d3ee7c559028a4
Hubert Smieciuch <hubert.smieciuch@gmail.com>
2022-11-17 00:10:24 +0100

    added functions for sf objects transformations

--- R/utils.R
+++ R/utils.R
@@ -1,6 +1,5 @@
 
 
-
 #' Scale data to be within given range
 #' @description Agregates data by given groups to averages
 #'
@@ -17,8 +16,6 @@ scale_data <- function(data, num_col, min=0, max=10){
   data_s <- data.table::copy(data) %>% .[, c(num_col):=lapply(.SD, function(x) round(scales::rescale(x, to=c(min, max)), 2)), .SDcols=num_col]
   return(data_s)
 }
-
-
 
 
 #' Get averages by groups
@@ -42,6 +39,7 @@ get_avg <- function(data, grp_col, val_col){
   data
 }
 
+
 #' Get mean, median or sd within groups
 #' @description Aggregates data by given groups to mean, median or sd
 #'
@@ -63,7 +61,6 @@ get_agr <- function(data, grp_col, val_col, agr_type="mean"){
     .[, lapply(.SD, function(x) round(agreg(x[!is.nan(x)], na.rm = T), 2)), by=c(grp_col, "agregat"), .SDcols=val_col] # compute means per group for each variable
   data
 }
-
 
 
 #' Get matrix of radius distances
@@ -93,7 +90,6 @@ get_rad_dist_matrix <- function(data1, lonlat1, data2=NULL, lonlat2=NULL){
 }
 
 
-
 #' Count number of points within radius distance
 #' @description Count number of points within radius distance of given number of kilometers
 #'
@@ -112,8 +108,6 @@ count_in_rad <- function(radius, data1, lonlat1, data2=NULL, lonlat2=NULL){
   res <- rowSums(dist_matrix[, lapply(.SD, function(x) x<radius*1000), .SDcols=names(dist_matrix)])
   return(res)
 }
-
-
 
 
 #' Find records within radius distance
@@ -147,7 +141,6 @@ get_n_in_rad <- function(radius, data1, id1, lonlat1, data2=NULL, id2=NULL, lonl
 }
 
 
-
 #' Find records given number of closests records from given one
 #'
 #' @param n number of closest locations to be returned
@@ -178,4 +171,88 @@ get_n_closest<- function(n, data1, id1, lonlat1, data2=NULL, id2=NULL, lonlat2=N
   names(res) <- data1[[id1]]
   return(res)
 }
+
+
+#' Crop and cast sf data.frame object
+#'
+#' @param sf_obj sf data.frame object
+#' @param bbox list containing: xmin, ymin, xmax, ymax that denote extent of cropping area
+#' @param spat_obj_type character, name of type of each geometry in sf data.frame (only needed if cropping causes ambiguity)
+#'
+#' @return sf object
+#' @export
+#'
+#' @examples
+crop_n_cast <- function(sf_obj, bbox, spat_obj_type=NULL){
+  sf_obj <- tryCatch({sf::st_crop(sf_obj, bbox)},
+                     warning=function(w){
+                       if(grepl("attribute variables are assumed to be spatially constant throughout all geometries", w$message) & !is.null(spat_obj_type)){
+                         return(suppressWarnings(sf::st_cast(sf::st_crop(sf_obj, bbox), spat_obj_type)))
+                       } else {
+                         return(NULL)
+                       }
+                     }
+  )
+  if(is.null(sf_obj)){
+    stop("Attribute variables are not spatially constant throughout all geometries, provide type they are to be cast to as 'spat_obj_type'")
+  }
+  return(sf_obj)
+}
+
+
+#' Transform data.frame or data.table to sf object
+#'
+#' @param data data.frame or data.table to be transformed to sf object
+#' @param lon character, longitude column name
+#' @param lat character, latitude column name
+#' @param crs EPSG CRS code of Coordinate Reference Systems
+#' @param bbox list containing: xmin, ymin, xmax, ymax that denote extent of cropping area
+#' @param spat_obj_type haracter, name of type of each geometry in sf data.frame (only needed if cropping causes ambiguity)
+#'
+#' @return sf object
+#' @export
+#'
+#' @examples
+prep_sf <- function(data, lon, lat, crs, bbox=NULL, spat_obj_type=NULL){
+  if(inherits(data, "sf")){
+    message("Data is already of 'sf' class.")
+    coords_sf <- data
+  } else {
+    coords_sf <- sf::st_as_sf(data, coords = c(lon, lat))
+  }
+  coords_sf <- sf::st_set_crs(coords_sf, crs)
+  if(!is.null(bbox)){
+    coords_sf <- crop_n_cast(coords_sf, bbox, spat_obj_type)
+  }
+  return(coords_sf)
+}
+
+
+#' Transform sf of data.frame to geojson of type FeatureCollection
+#' @description Transform sf of data.frame to geojson of type FeatureCollection used for drawing highcharter map.
+#'
+#' @param data data.frame, data.table or sf frame containing data
+#' @param lon character, longitude column name
+#' @param lat character, latitude column name
+#' @param crs EPSG CRS code of Coordinate Reference Systems
+#' @param bbox list containing: xmin, ymin, xmax, ymax that denote extent of cropping area
+#' @param spat_obj_type haracter, name of type of each geometry in sf data.frame (only needed if cropping causes ambiguity)
+#'
+#' @return FeatureCollection object
+#' @export
+#'
+#' @examples
+prep_geojson <- function(data, lon, lat, crs, bbox=NULL, spat_obj_type=NULL){
+  if(!inherits(data, "sf")){
+    coords_sf <- prep_sf(data, lon, lat, crs, bbox, spat_obj_type)
+  } else {
+    coords_sf <- data
+  }
+  if(!is.null(bbox) & !is.null(spat_obj_type)){
+    coords_sf <- crop_n_cast(coords_sf, bbox, spat_obj_type)
+  }
+  mappoint_geojson <-  geojsonio::geojson_json(coords_sf, lat = lat, lon = lon)
+  return(mappoint_geojson)
+}
+
 
